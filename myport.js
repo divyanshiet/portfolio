edcb30afa1fa77040f00ca6a5a4a51e45c690be7
divyansh pandey <98694994+divyanshiet@users.noreply.github.com>
2022-10-13 23:12:11 +0530

    js

---
+++ myport.js
@@ -0,0 +1,58 @@
+document.querySelector("#about").addEventListener("click",function(){
+    alert('scroll down');
+});
+document.getElementById("kk").addEventListener("mouseout",function(){
+    document.querySelector("#info1").className="info1";
+    
+});
+document.getElementById("kk").addEventListener("mouseout",function(){
+    document.querySelector("#content").className="content";
+});
+
+document.getElementById("kk").addEventListener("mouseout",function(){
+    document.querySelector("#name").className="name";
+});
+document.querySelector("#about").addEventListener("mouseout",function(){
+    document.querySelector("#info2").className="info2";
+});
+
+document.getElementById("alert").addEventListener("click",function(){
+    alert("to know anything,mouseover on nav tab")
+});
+document.querySelector("#hi").addEventListener("mouseover",function(){
+    document.querySelector("#haha").className="haha";
+    document.querySelector("#info1").style.color="black";
+    document.querySelector("#content").style.color="black";
+    document.querySelector("#name").style.color="black";
+    document.querySelector("#info2").style.color="black";
+    document.querySelector("#intro").style.color="black";
+    document.querySelector("#abc").style.color="black";
+    document.querySelector("#it").style.color="black";
+    document.querySelector("#port").style.color="black";
+    document.querySelector("#folio").style.color="black";
+    document.querySelector("#contact").style.color="black";
+    document.querySelector("#ram").style.color="black";
+    document.querySelector("#gmail").style.color="black";
+    document.querySelector("#instagram").style.color="black";
+    document.querySelector("#github").style.color="black";
+
+
+
+})
+document.querySelector("#ok").addEventListener("mouseover",function(){
+    document.querySelector("#quit").className="quit";
+    document.querySelector("#info1").style.color="black";
+    document.querySelector("#content").style.color="black";
+    document.querySelector("#name").style.color="black";
+    document.querySelector("#info2").style.color="black";
+    document.querySelector("#intro").style.color="black";
+    document.querySelector("#abc").style.color="black";
+    document.querySelector("#it").style.color="black";
+    document.querySelector("#port").style.color="black";
+    document.querySelector("#folio").style.color="black";
+    document.querySelector("#contact").style.color="black";
+    document.querySelector("#ram").style.color="black";
+    document.querySelector("#gmail").style.color="black";
+    document.querySelector("#instagram").style.color="black";
+    document.querySelector("#github").style.color="black";
+});
